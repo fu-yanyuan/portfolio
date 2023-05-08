a7dc054d92edc687ed4484eb374c096237847dec
fu <yanyuan@g.ecc.u-tokyo.ac.jp>
2023-05-09 02:19:10 +0900

    fix: modify about page for my iphone8

--- pages/components/about.js
+++ pages/components/about.js
@@ -7,13 +7,13 @@ export default function About({darkMode}) {
             About
           </p>
         </div>
-        <p className="text-xl leading-loose text-gray-800 mt-20 dark:text-slate-300"> 
+        <p className="text-xl leading-loose text-gray-800 mt-5 sm:mt-20 dark:text-slate-300"> 
           I am currently a Master's student in the Graduate School of Information Science and Technology at <a className="text-black font-bold cursor-pointer hover:underline dark:text-white" href="https://www.u-tokyo.ac.jp/en/index.html">the University of Tokyo</a>. 
           As a member of <a className="text-black font-bold cursor-pointer hover:underline dark:text-white" href="http://www.hal.t.u-tokyo.ac.jp/lab/en/index_1.xhtml">Aizawa-Yamakata-Matsui Lab</a>, 
           I am supervised by <a className="text-black font-bold cursor-pointer hover:underline dark:text-white" href="https://yusukematsui.me/index.html">Yusuke Matsui</a>.
           Prior to this, I obtained my Bachelor's degree from <a className="text-black font-bold cursor-pointer hover:underline dark:text-white" href="https://en.wikipedia.org/wiki/Harbin_Institute_of_Technology">Harbin Institute of Technology</a>. 
         </p>
-        <p className="text-xl leading-loose text-gray-800 mt-20 dark:text-slate-300">
+        <p className="text-xl leading-loose text-gray-800 mt-5 sm:mt-20 dark:text-slate-300">
           <span className="text-black font-bold dark:text-white">When I'm not in front of a computer</span>, 
           I'm probably playing soccer on the field <span className="text-3xl">⚽</span>. 
           Besides, I like hiking <span className="text-3xl">⛰</span>.
